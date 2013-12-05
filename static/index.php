42105e44445ede0ffcb2ba5ba13fa227903e2fee
Alexandre Leclair <alexandre.leclair@zloche.net>
2013-12-05 13:13:27 -0500

    calice

--- static/index.php
+++ static/index.php
@@ -149,8 +149,8 @@ Y,     ,d88P
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
